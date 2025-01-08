60091ed6066ce1c6988ede955d4fe5dce06fc91f
ujjwal Kumar Bhardwaj <ujjwalmehsi17@gmail.com>
2025-01-09 02:00:17 +0530

    Removed Unused files

--- app/src/main/java/com/ujjwal/stepathonanimation/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/ujjwal/stepathonanimation/onboarding/OnBoardingViewModel.kt
@@ -7,7 +7,6 @@ import com.ujjwal.stepathonanimation.R
 import com.ujjwal.stepathonanimation.onboarding.utils.Event
 
 class OnBoardingViewModel : ViewModel() {
-    val topImages = listOf(R.drawable.robot, R.drawable.trophy, R.drawable.gifts)
     private val _pageNumber: MutableLiveData<Event<Int>> = MutableLiveData(Event(0))
     val pageNumber: LiveData<Event<Int>> get() = _pageNumber
 
